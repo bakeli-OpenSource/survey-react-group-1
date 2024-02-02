64f2c803cff0bb00eb92594ad04118956ddc06b0
Pathé <zalou2016@gmail.com>
2024-02-02 18:33:55 +0000

    Auth

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,14 +1,67 @@
 import {useState} from "react";
+import axios from "axios";
+import { useLocation } from 'react-router-dom';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav"
-import { Link } from "react-router-dom";
+import { Link,Navigate } from "react-router-dom";
 import { Col, Container, Row } from "react-bootstrap";
 import imgLogin from "../../components/images/login.png";
 import logoSmall from "../../components/images/logo-small.png";
 import './login.css'
 
 const Login = () => {
+
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const successMessage = queryParams.get('successMessage');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  // const [successSms, setSuccessSms] = useState(null);
+  const [isLogged, setIsLogged] = useState(false); // Etat pour gérer la redirection
+  // const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/login', {
+        email: email,
+        password: password
+      });
+
+      // Traitez la réponse en cas de succès
+      console.log('Réponse de la requête POST:', response.data);
+      // setSuccessSms('Connexion réussie !');
+      // Effacez les éventuels messages d'erreur précédents
+      setError(null);
+      // Effacez les champs du formulaire après une inscription réussie
+      setEmail('');
+      setPassword('');
+      setIsLogged(true);
+      // setSuccessSms('Connexion réussie !');
+      // navigate("/login")
+    } catch (error) {
+      if (error.response) {
+        // Si la réponse est reçue mais avec un statut d'erreur
+        console.error('Erreur de la requête:', error.response.data);
+        setError(error.response.data.message); // Enregistrez le message d'erreur dans l'état
+        // Effacez les éventuels messages de succès précédents
+      } else if (error.request) {
+        // Si la requête est faite mais aucune réponse n'est reçue
+        console.error('Pas de réponse de la requête:', error.request);
+        // setSuccessSms(null);
+      } else {
+        // Autres erreurs
+        console.error('Erreur lors de la requête:', error.message);
+      }
+    }
+  };
+  if (isLogged) {
+    return <Navigate to="/home-dash"/>;
+  }
+
   return (
     <div className="container my-3" style={{backgroundColor:''}}>
      
@@ -23,7 +76,10 @@ const Login = () => {
             margin:'70px auto',
         }}/>
           </div>
-          </Link>
+        </Link>
+        {error && <p style={{ color: 'red' }} className="text-center"><strong>{error}</strong></p>} {/* Affichez l'erreur si elle existe */}
+        {successMessage && <p style={{ color: 'green'}} className="text-center"><strong>{successMessage}</strong></p>}
+        {/* {successSms && <p style={{ color: 'green'}} className="text-center"><strong>{successSms}</strong></p>} */}
         <Row className="container-2">
         <Col md={6}>
             <div className="head_right">
@@ -36,11 +92,16 @@ const Login = () => {
             </div>
           </Col>
           <Col md={6} className="">
-            <Form className="" style={{marginTop:'95px'}} >
+            <Form className="" style={{marginTop:'95px'}} onSubmit={handleSubmit}>
                 {/* <h2 className="">Se connecter</h2> */}
               <Form.Group className="mb-3" controlId="formBasicEmail">
                 {/* <Form.Label>Email address</Form.Label> */}
-                <Form.Control type="email" className="" style={{
+                <Form.Control type="email"
+                  id='email'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                 className="" 
+                 style={{
                     border:'none',
                     padding:'12px',
                     width:'70%',
@@ -53,7 +114,11 @@ const Login = () => {
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 {/* <Form.Label>Password</Form.Label> */}
-                <Form.Control type="password" style={{
+                <Form.Control type="password" 
+                id='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{
                     border:'none',
                     padding:'12px',
                     width:'70%',
--- src/components/Pages/Register.jsx
+++ src/components/Pages/Register.jsx
@@ -3,45 +3,97 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
 
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { Col, Container, Row, Toast } from "react-bootstrap";
 import imgRegister from "../../components/images/register.png";
-import logo from "../../components/images/logo.png";
+// import logo from "../../components/images/logo.png";
 import logoSmall from "../../components/images/logo-small.png";
 import "./login.css";
 import axios from "axios";
-import toast, { Toaster } from 'react-hot-toast';
-import { useNavigate } from "react-router-dom";
+// import toast, { Toaster } from 'react-hot-toast';
+// import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [isRegistered, setIsRegistered] = useState(false); // Etat pour gérer la redirection
+  // const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/register', {
+        name: name,
+        email: email,
+        password: password
+      });
+
+      // Traitez la réponse en cas de succès
+      console.log('Réponse de la requête POST:', response.data);
+      setSuccessMessage('Inscription réussie ! Vous pouvez maintenant vous connecter.');
+      // Effacez les éventuels messages d'erreur précédents
+      setError(null);
+      // Effacez les champs du formulaire après une inscription réussie
+      setName('');
+      setEmail('');
+      setPassword('');
+      setIsRegistered(true);
+      setSuccessMessage('Inscription réussie ! Vous pouvez maintenant vous connecter.');
+      // navigate("/login")
+    } catch (error) {
+      if (error.response) {
+        // Si la réponse est reçue mais avec un statut d'erreur
+        console.error('Erreur de la requête:', error.response.data);
+        setError(error.response.data.message); // Enregistrez le message d'erreur dans l'état
+        // Effacez les éventuels messages de succès précédents
+      } else if (error.request) {
+        // Si la requête est faite mais aucune réponse n'est reçue
+        console.error('Pas de réponse de la requête:', error.request);
+        setSuccessMessage(null);
+      } else {
+        // Autres erreurs
+        console.error('Erreur lors de la requête:', error.message);
+      }
+    }
+  };
+  if (isRegistered) {
+    return <Navigate to="/login?successMessage=Inscription%20réussie%20!%20Vous%20pouvez%20maintenant%20vous%20connecter"/>;
+  }
   return (
     <div className="container my-3"
         style={{ backgroundColor: "", padding: "2px" }}>
           
       <Container className="content">
-        <Toaster/>
         <Link to="/">
           <div className="container row" style={{}}>
       
-          <img  src={logoSmall} alt="logo" style={{
-            width:'15%',
-            marginLeft:'550px',
-            margin:'60px auto',
-          }}/>
+            <img  src={logoSmall} alt="logo" style={{
+              width:'15%',
+              marginLeft:'550px',
+              margin:'60px auto',
+            }}/>
           </div>
-          </Link>
+        </Link>
+        {error && <p style={{ color: 'red' }} className="text-center"><strong>{error}</strong></p>} {/* Affichez l'erreur si elle existe */}
           <Row className="container-2">
             <Col md={6}>
-              
               <Form className="" style={{ 
                 marginTop: "75px",
                 marginLeft:'100px'
-                }}>
+                }}
+                onSubmit={handleSubmit}>
                 <Form.Group className="" controlId="formBasicEmail">
                   {/* <Form.Label>Email address</Form.Label> */}
                   <Form.Control
                     type="text"
                     className=""
+                    id='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     style={{
                       border: "none",
                       padding: "13px",
@@ -54,6 +106,9 @@ const Register = () => {
                   <Form.Control
                     type="email"
                     className=""
+                    id='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     style={{
                       border: "none",
                       padding: "13px",
@@ -64,8 +119,11 @@ const Register = () => {
                 </Form.Group> <br />
 
                   <Form.Control
-                    name="password"
+                    // name="password"
                     type="password"
+                    id='name'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     style={{
                       border: "none",
                       padding: "13px",
